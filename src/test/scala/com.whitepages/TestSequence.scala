c33d405761146797427cf287712c3828f36729d3
ecarlson <ecarlson@whitepages.com>
2014-10-02 11:07:51 -0700

    test cleanup

--- src/test/scala/com.whitepages/TestSequence.scala
+++ src/test/scala/com.whitepages/TestSequence.scala
@@ -1,19 +1,10 @@
 package com.whitepages
 
-import java.util.concurrent.atomic.AtomicBoolean
-
-import scalaz.{\/-, -\/}
-
 import org.scalatest.{FlatSpec, Matchers}
-import shapeless._
-import scala.concurrent.duration._
-import scalaz._
-import shapeless.contrib.scalaz.sequence
 
-import scalaz.-\/
-import scalaz.concurrent.{Task, Future}
-import scalaz.\/._
-import PlanStep._
+import scala.concurrent.duration._
+import scalaz.{-\/, \/-}
+import scalaz.concurrent.Future
 
 class TestSequence extends FlatSpec with Matchers {
   "sequence" should "fail fast" in {
@@ -44,23 +35,6 @@ class TestSequence extends FlatSpec with Matchers {
 
     cOut should equal(1, "hi")
     warnings.map(_.msg) should equal(List("1", "2", "3"))
-//    val d = sequence(a :: b :: HNil).map { case aOut :: bOut :: HNil => (aOut, bOut) }
-//
   }
 
-  "multiple callbacks" should "only honor the first one" in {
-    val start = System.currentTimeMillis()
-
-    val short = Future.schedule("short", 1.milliseconds)
-    val long = Future.schedule("long", 100.milliseconds)
-    val a: Future[String] = Future.Async { cb =>
-      short.runAsync(s => cb(s))
-      long.runAsync(s => cb(s))
-    }
-    val res = a.run
-
-    val elapsed = System.currentTimeMillis - start
-    res should equal("short")
-    assert(elapsed < 100, "elapsed time should be < 100 milliseconds")
-  }
 }
